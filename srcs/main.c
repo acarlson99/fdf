f2f6bc8f540f4d6fe14bf7e400c5bca819a16fe1
Alexander Carlson <acarlson@e1z3r1p23.42.us.org>
2018-12-31 15:03:56 -0800

    Updated macros and checks in main

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/15 14:10:38 by acarlson          #+#    #+#             */
-/*   Updated: 2018/12/31 00:39:47 by acarlson         ###   ########.fr       */
+/*   Updated: 2018/12/31 15:01:23 by acarlson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,19 @@
 
 void	ohfuck(t_fdf *info, int code)
 {
-	if (code == 1)
+	if (code == USG_ERR)
 	{
 		if (info)
 			ft_printf("usage: %s <file> [{bold}p{nc}arallel {bold}i{nc}so] \
 [modify_projection_size]\n", info->exec_name);
 	}
-	else if (code == 2)
+	else if (code == FILE_ERR)
 		ft_printf("file read error\n");
-	else if (code == 3)
+	else if (code == PARS_ERR)
 		ft_printf("file parsing error\n");
-	else if (code == 4)
+	else if (code == MLX_ERR)
 		ft_printf("unable to create window\n");
-	else if (code == 5)
+	else if (code == WIN_ERR)
 		ft_printf("unable to create window\n");
 	exit(1);
 }
@@ -49,9 +49,9 @@ int		main(int argc, char **argv)
 	if (code)
 		ohfuck(info, code);
 	if (!info->mlx_ptr)
-		ohfuck(info, 3);
+		ohfuck(info, MLX_ERR);
 	else if (!info->win_ptr)
-		ohfuck(info, 4);
+		ohfuck(info, WIN_ERR);
 	draw_image(info);
 	mlx_key_hook(info->win_ptr, deal_key, (void *)0);
 	mlx_loop(info->mlx_ptr);
