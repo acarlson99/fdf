00c7821b60803814c0f8ebfc81ba76083a44f96b
Alexander Carlson <acarlson@e1z3r1p23.42.us.org>
2018-12-31 16:12:05 -0800

    Fixed colors

--- srcs/draw_image.c
+++ srcs/draw_image.c
@@ -6,15 +6,22 @@
 /*   By: acarlson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/21 19:39:33 by acarlson          #+#    #+#             */
-/*   Updated: 2018/12/31 00:48:07 by acarlson         ###   ########.fr       */
+/*   Updated: 2018/12/31 16:09:19 by acarlson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
+#define ISO_RED(n) ((n & 0xFF0000) >> 4)
+#define ISO_GRN(n) ((n & 0x00FF00) >> 2)
+#define ISO_BLU(n) (n & 0x0000FF)
+
 void		draw_line_color_gradient(t_fdf *info, t_fvec *v, t_fvec *w)
 {
-	draw_line_bresenham(info, v->v, w->v, (v->color + w->color) / 2);
+	draw_line_bresenham(info, v->v, w->v,								\
+						(((ISO_RED(v->color) + ISO_RED(w->color)) / 2) << 4) \
+						+ (((ISO_GRN(v->color) + ISO_GRN(w->color)) / 2) << 2) \
+						+ (((ISO_BLU(v->color) + ISO_BLU(w->color)) / 2)));
 }
 
 void		conv(t_fdf *info)
